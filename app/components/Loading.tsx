ec73a8bf3e1babdf32bd97952e5653d32e62c026
Richard-Abraham <richardabraham.kay.37@gmail.com>
2024-11-15 23:07:05 +0300

    update

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -14,7 +14,7 @@ export function Loading() {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="absolute inset-0 bg-gradient-to-br from-blue-600 via-purple-600 to-red-600 animate-gradient-slow" />
-      <div className="absolute inset-0 bg-white/50 dark:bg-gray-950/50 backdrop-blur-3xl" />
+      <div className="absolute inset-0 bg-[#FEFFFF]/50 dark:bg-gray-950/50 backdrop-blur-3xl" />
       <div className="relative text-center space-y-4">
         <l-superballs
           size="50"
--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen relative">
-      <div className="absolute inset-0 bg-gradient-to-br from-white via-blue-50 to-white dark:from-blue-950 dark:via-purple-950 dark:to-gray-900 -z-10 animate-gradient-slow fixed" />
+      <div className="absolute inset-0 bg-gradient-to-br from-[#FEFFFF] via-blue-50 to-[#FEFFFF] dark:from-blue-950 dark:via-purple-950 dark:to-gray-900 -z-10 animate-gradient-slow fixed" />
       <div className="absolute inset-0 bg-white/50 dark:bg-gray-950/50 backdrop-blur-3xl -z-5 fixed" />
       <Header isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} activeSection={activeSection} />
       <main className="relative z-[2]">
