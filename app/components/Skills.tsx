a6a28d030f547e8eb362c1f787de12fd351a8646
Richard-Abraham <richardabraham.kay.37@gmail.com>
2025-08-04 00:51:28 +0300

    update

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect } from "react"
+import React from "react"
 import { 
   SiHtml5, SiCss3, SiJavascript, SiTypescript, 
   SiReact, SiNextdotjs, SiNodedotjs, SiExpress, 
